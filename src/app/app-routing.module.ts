0727c9ce322166719602c299a77b95c77369989c
小小一小只 <wang1015519614@gmail.com>
2021-09-06 17:36:25 +0800

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { heroesComponent } from './heroes/heroes.component';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: '/dashboard', pathMatch: 'full' },
+  { path: 'heroes', component: heroesComponent },
+  { path: 'dashboard', component: DashboardComponent },
+  { path: 'detail/:id', component: HeroDetailComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,26 @@
+import { MessageService } from './../message.service';
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class heroesComponent implements OnInit {
+
+  constructor(private heroService: HeroService, private messageService: MessageService) { }
+
+  heroes: Hero[] = [];
+
+  getHeroes(): void {
+    this.heroService.getHeroes().subscribe(
+      heroes => this.heroes = heroes
+    );
+  }
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+}
